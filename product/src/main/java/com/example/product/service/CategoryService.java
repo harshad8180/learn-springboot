00c7311de63d4c454f7ef71f51e693fec2f7b1bc
harshad8180 <hpborkar@gmail.com>
2025-05-23 16:58:51 +0530

    implemented create category service

--- product/src/main/java/com/example/product/service/CategoryService.java
+++ product/src/main/java/com/example/product/service/CategoryService.java
@@ -2,7 +2,25 @@ package com.example.product.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.product.dto.CategoryDTO;
+import com.example.product.entity.Category;
+import com.example.product.mapper.CategoryMapper;
+import com.example.product.repository.CategoryRepository;
+
 @Service
 public class CategoryService {
-
+	private CategoryRepository categoryRepository;
+	
+	// create category
+	public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+		Category category = CategoryMapper.toCategoryEntity(categoryDTO);
+		category = categoryRepository.save(category);
+		return CategoryMapper.toCategoryDTO(category);
+	}
+	
+	// get all categories
+	
+	// get category by id
+	
+	// delete category
 }
