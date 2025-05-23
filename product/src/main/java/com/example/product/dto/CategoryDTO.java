365b7156aceedcbe69293d6f0388ae1a0ce23c19
harshad8180 <hpborkar@gmail.com>
2025-05-23 14:59:00 +0530

    dto implemented

--- product/src/main/java/com/example/product/dto/CategoryDTO.java
+++ product/src/main/java/com/example/product/dto/CategoryDTO.java
@@ -1,5 +1,16 @@
 package com.example.product.dto;
 
-public class CategoryDTO {
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+	private Long id;
+	private String name;
+	private List<ProductDTO> products;
 }
