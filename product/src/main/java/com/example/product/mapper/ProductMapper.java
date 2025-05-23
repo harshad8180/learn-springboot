7be512529a6025054058bc7455a7ec168c25a4d4
harshad8180 <hpborkar@gmail.com>
2025-05-23 15:29:58 +0530

    mapped entity and dto

--- product/src/main/java/com/example/product/mapper/ProductMapper.java
+++ product/src/main/java/com/example/product/mapper/ProductMapper.java
@@ -1,5 +1,30 @@
 package com.example.product.mapper;
 
+import com.example.product.dto.ProductDTO;
+import com.example.product.entity.Category;
+import com.example.product.entity.Product;
+
 public class ProductMapper {
 
+	// entity to DTO
+	public static ProductDTO toProductDTO(Product product) {
+		return new ProductDTO(
+				product.getId(),
+				product.getName(),
+				product.getDescription(),
+				product.getPrice(),
+				product.getCategory().getId()
+				);
+	}
+	
+	// DTO to entity
+	public static Product toProductEntity (ProductDTO productDTO, Category category) {
+		Product product = new Product();
+		product.setName(productDTO.getName());
+		product.setDescription(productDTO.getDescription());
+		product.setPrice(productDTO.getPrice());
+		product.setCategory(category);
+		
+		return product;
+	}
 }
